fa265d1403a1ab171bb49559796ccc44b89e1e15
john32y <john.thelronge@gmail.com>
2023-07-07 12:38:58 -0400

    Updated Chekov

--- src/app/TodoListitem.jsx
+++ src/app/TodoListitem.jsx
@@ -1,8 +1,15 @@
 export default function TodoListitem({ item}) {
     return(
-        <li className="flex items-center p-4 cursor-pointer select-none">
-        <div className="">
+        <li className="flex items-center p-4 cursor-pointer select-none hover:bg-violet-600">
+        <div className="flex-1 pl-1 mr-16 font-medium text-zinc-50 text-left">
             {item.title}
+        </div>
+        <div className="w-10 h-10 mr-4">
+        <svg class="w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" width="6" height="6" stroke="currentColor" fill="#10b981" viewBox="0 0 1792 1792">
+                <path d="M1412 734q0-28-18-46l-91-90q-19-19-45-19t-45 19l-408 407-226-226q-19-19-45-19t-45 19l-91 90q-18 18-18 46 0 27 18 45l362 362q19 19 45 19 27 0 46-19l543-543q18-18 18-45zm252 162q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z">
+                </path>
+            </svg>
+
         </div>
         </li>
     )
